b9ab22ff39d18cd677041fd08fcc8323155f3648
Smit Patel <smit.patel@teamviewer.com>
2024-02-16 00:14:59 +0100

    MUI added and started to work on compoennts

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import { Toolbar } from "@mui/material";
+import { Typography } from "@mui/material";
+import "./NavBar.styles.css";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <AppBar
+      position="static"
+      color="info"
+    >
+      <Toolbar>
+        <Typography
+          variant="h6"
+          className="navbar-title"
+        >
+          Visualization
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;

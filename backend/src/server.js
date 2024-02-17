8e757220dd520757ab6681d55f1a2270b394ed26
Smit Patel <smit.patel@teamviewer.com>
2024-02-17 13:17:40 +0100

    server.js/now files will be saved with the name and extention

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const upload = multer({
-  dest: "uploads/",
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extension = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, extension);
+    const newFilename = `${basename}-${uniqueSuffix}${extension}`;
+    cb(null, newFilename);
+    console.log(`Mapping: ${file.originalname} -> ${newFilename}`);
+  },
 });
+
+const upload = multer({ storage: storage });
+
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow only your frontend origin
-  optionsSuccessStatus: 200, // For legacy browser support
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); // Use CORS with options
+app.use(cors(corsOptions));
 app.post("/upload", upload.array("files[]"), (req, res) => {
   console.log(req.files);
-  res.json({ message: "Files uploaded successfully" }); // Send a JSON response
+  res.json({ message: "Files uploaded successfully" });
 });
 
 const PORT = process.env.PORT || 3001;

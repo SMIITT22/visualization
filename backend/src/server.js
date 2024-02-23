e6594103549ecefc8d27a298fa7da541101808f4
Smit Patel <smit.patel@teamviewer.com>
2024-02-24 00:23:24 +0100

    subfolder added for each user input

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
+//here is the storing functionality.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "../uploads/");
   },
   filename: function (req, file, cb) {
     const extension = path.extname(file.originalname);
@@ -18,6 +20,7 @@ const storage = multer.diskStorage({
   },
 });
 
+//here we are Filtering the src folder which is submitted by user.
 const fileFilter = (req, file, cb) => {
   if (/\.(jsx|tsx|js|ts)$/.test(file.originalname)) {
     cb(null, true);
@@ -46,19 +49,38 @@ app.listen(PORT, () => {
 const { processFiles } = require("./utils/fileProcessor");
 const { buildTree } = require("./utils/HierarchyBuilder");
 
+const { v4: uuidv4 } = require("uuid"); // Ensure you've installed uuid
+
 app.post("/upload", upload.array("files[]"), async (req, res) => {
-  const directory = path.join(__dirname, "../uploads");
-  const componentsWithImports = await processFiles(directory);
-  console.log("componentsWithImports", componentsWithImports);
+  const uploadSessionId = uuidv4();
+  const directory = path.join(__dirname, "../uploads", uploadSessionId);
 
-  const rootComponentName = "index.js";
-  const tree = buildTree(rootComponentName, componentsWithImports);
+  try {
+    // Ensure the subfolder exists
+    fs.mkdirSync(directory, { recursive: true });
 
-  // res.json({
-  //   message: "Upload and processing completed",
-  //   data: componentsWithImports,
-  // });
-  res.json({ message: "Upload and processing completed", tree });
+    // Move uploaded files into the subfolder
+    req.files.forEach((file) => {
+      // Construct the path where the file will be saved
+      const savePath = path.join(directory, file.originalname);
+      // Move the file from the temporary location to the save path
+      fs.renameSync(file.path, savePath);
+    });
 
-  console.log("tree", tree);
+    const componentsWithImports = await processFiles(directory);
+    console.log("componentsWithImports", componentsWithImports);
+
+    const rootComponentName = "index.js";
+    const tree = buildTree(rootComponentName, componentsWithImports);
+
+    // res.json({
+    //   message: "Upload and processing completed",
+    //   data: componentsWithImports,
+    // });
+    console.log("tree", tree);
+    res.json({ message: "Upload and processing completed", tree });
+  } catch (error) {
+    console.error("Error handling upload from server:", error);
+    res.status(500).send({ message: "Failed to handle upload from server" });
+  }
 });
